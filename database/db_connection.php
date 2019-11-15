cc301b4dfd634f94c469b0ca1f83ef9b8bf0c085
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2019-11-15 10:34:41 -0500

    working registration with database

--- database/db_connection.php
+++ database/db_connection.php
@@ -2,7 +2,7 @@
 
 $dbServername = 'localhost';
 $dbUsername = 'root';
-$dbPassword = '';
+$dbPassword = 'root';
 $dbDataBaseName = 'micasa';
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbDataBaseName) or die("<h1>Could not connect to database.</h1>");
