1eee829909c45dffca4b422052bba00297ae0c12
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2019-11-22 17:07:16 -0500

    snother db insert fix

--- database/db_inserts.sql
+++ database/db_inserts.sql
@@ -1,5 +1,5 @@
 INSERT INTO `register`(`RegID`,`FirstName`, `LastName`, `Email`, `Telephone`, `Username`, `Password`, `Password2`) 
-VALUES (‘023','John','Brown','johnbrown@gmail.com','8763996444','johnb','password','password');
+VALUES (‘0','John','Brown','johnbrown@gmail.com','8763996444','johnb','password','password');
 
 
 INSERT INTO `property`(`PropertyID`, `Address1`, `Address2`, `City`, `Parish`, `Size`, `ListingType`, `PropertyType`, `BuildingType`, `NumBedroom`, `NumBathroom`, `Price`) 
