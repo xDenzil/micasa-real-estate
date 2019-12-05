e615d00e265624ec90f6a7d7afce14e6635de3ed
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2019-12-04 21:42:20 -0500

    database update

--- database/db_connection.php
+++ database/db_connection.php
@@ -1,6 +1,6 @@
 <?php
-$dbServername = 'localhost';
-$dbUsername = 'root';
-$dbPassword = 'root';
-$dbDataBaseName = 'mi_casa';
+$dbServername = 'us-cdbr-iron-east-05.cleardb.net';
+$dbUsername = 'b943ed4a3fffdf';
+$dbPassword = 'e710490b';
+$dbDataBaseName = 'heroku_d9252a22c498083';
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbDataBaseName) or die("<h1>Could not connect to database.</h1>");
