35446e14fbdad90276e668a4f633e6cdff800534
Denzil Williams <denzzilwilliams@gmail.com>
2020-06-27 17:14:00 -0500

    test

--- database/db_connection.php
+++ database/db_connection.php
@@ -2,5 +2,5 @@
 $dbServername = 'hcm4e9frmbwfez47.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $dbUsername = 'tohq3fdc0814quc5';
 $dbPassword = 'obqpfwfm1h2sja70';
-$dbDataBaseName = 'xw0zyspwbodm9m25';
+$dbDataBaseName = 'xw0zyspwbodm9m2';
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbDataBaseName) or die("<h1>Could not connect to database.</h1>");
